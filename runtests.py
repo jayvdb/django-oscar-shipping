acdf40244e86af689f06252ecf161bd3ea245122
John Vandenberg <jayvdb@gmail.com>
2020-06-01 09:55:46 +0700

    runtests: Add settings needed to run Oscar

--- runtests.py
+++ runtests.py
@@ -1,7 +1,9 @@
+import os.path
 import sys
 
 try:
     from django.conf import settings
+    from django.urls import reverse_lazy
 
     settings.configure(
         DEBUG=True,
@@ -16,8 +18,56 @@ try:
             "django.contrib.auth",
             "django.contrib.contenttypes",
             "django.contrib.sites",
+            "oscar.apps.address",
+            "oscar.apps.catalogue",
+            "oscar.apps.catalogue.reviews",
+            "oscar.apps.customer",
+            'oscar.apps.search',
+            'oscar.apps.voucher',
+            'oscar.apps.payment',
+            "oscar.apps.basket",
+            "oscar.apps.partner",
+            "oscar.apps.order",
+            "oscar.apps.offer",
+            "oscar.apps.checkout",
             "oscar_shipping",
+            "test_project.shipping",
+            "oscar.apps.wishlists",
+            # Oscar dependencies
+            "widget_tweaks",
+            "haystack",
+            "treebeard",
+            "sorl.thumbnail",
+            "django_tables2",
         ],
+        OSCAR_DYNAMIC_CLASS_LOADER='oscar.core.loading.default_class_loader',
+        OSCAR_SLUG_FUNCTION = 'oscar.core.utils.default_slugifier',
+        OSCAR_SLUG_MAP = {},
+        OSCAR_SLUG_BLACKLIST = [],
+        OSCAR_HOMEPAGE = reverse_lazy('catalogue:index'),
+        OSCAR_IMAGE_FOLDER = 'images/products/%Y/%m/',
+        OSCAR_SLUG_ALLOW_UNICODE = False,
+        OSCAR_DELETE_IMAGE_FILES = True,
+        OSCAR_REQUIRED_ADDRESS_FIELDS = ('first_name', 'last_name', 'line1',
+                                 'line4', 'postcode', 'country'),
+        OSCAR_SEARCH_FACETS={'queries':{},'fields':{}},
+        OSCAR_OFFERS_PER_PAGE = 20,
+        OSCAR_PRODUCTS_PER_PAGE = 20,
+        OSCAR_REVIEWS_PER_PAGE = 20,
+        OSCAR_NOTIFICATIONS_PER_PAGE = 20,
+        OSCAR_EMAILS_PER_PAGE = 20,
+        OSCAR_ORDERS_PER_PAGE = 20,
+        OSCAR_ADDRESSES_PER_PAGE = 20,
+        OSCAR_STOCK_ALERTS_PER_PAGE = 20,
+        OSCAR_DASHBOARD_ITEMS_PER_PAGE = 20,
+        OSCAR_EAGER_ALERTS = False,
+        OSCAR_ACCOUNTS_REDIRECT_URL = 'customer:profile-view',
+        OSCAR_HIDDEN_FEATURES= [],
+        HAYSTACK_CONNECTIONS = {
+            'default': {
+                 'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
+            },
+        },
         SITE_ID=1,
         NOSE_ARGS=['-s'],
     )
